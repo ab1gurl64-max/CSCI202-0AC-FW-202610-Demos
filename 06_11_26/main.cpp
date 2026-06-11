ed88f6553ee5cd87def24be030f8163b31f56a12
Angela Venable <avenable11@ivytech.edu>
2026-06-11 17:54:54 -0400

    pointer demo

--- 06_11_26/main.cpp
+++ 06_11_26/main.cpp
@@ -5,9 +5,165 @@
 #include "order.h"
 
 // screenshot of your group on the people page in IvyLearn
+void codeGradeLoopFix(std::string errLocation); // fixes infinite loops
+void resetStream();
+std::string getIcing();
+std::string getTopping(std::string);
+std::string getDrizzle();
+void icingPrompt();
+void drizzlePrompt();
+void toppingPrompt(std::string);
+
+void referencePointers(Donut *&, Donut *);
 
 int main()
 {
+    std::string icing = getIcing();
+    std::string topping = getTopping(icing);
+    std::string drizzle = getDrizzle();
+
+    Donut *pointer1;
+    Donut myDonut(icing, topping, drizzle);
+    pointer1 = new Donut();
+    referencePointers(pointer1, &myDonut);
     Order donutOrder;
+    donutOrder + myDonut;
+    donutOrder + *pointer1;
+    std::cout << donutOrder;
+
     return 0;
+}
+
+void codeGradeLoopFix(std::string errLocation)
+{
+    if (std::cin.eof())
+    {
+        std::cout << "There was a problem and there is no more input! @" + errLocation << std::endl;
+        throw std::invalid_argument(errLocation);
+    }
+}
+
+std::string getIcing()
+{
+    std::string ice;
+    icingPrompt();
+    std::cin >> std::ws;
+    std::getline(std::cin, ice);
+    std::string icingLower = ice;
+    std::transform(icingLower.begin(), icingLower.end(), icingLower.begin(), ::tolower);
+    while (!Donut::strToice.count(icingLower))
+    {
+        codeGradeLoopFix("main.cpp::getIcing()");
+        // resetStream();
+        std::cout << ice << " is not a valid choice. " << std::endl;
+        icingPrompt();
+        std::cin >> std::ws;
+        std::getline(std::cin, ice);
+        icingLower = ice;
+        std::transform(icingLower.begin(), icingLower.end(), icingLower.begin(), ::tolower);
+    }
+    return ice;
+}
+
+std::string getTopping(std::string icing)
+{
+    std::string top;
+    std::transform(icing.begin(), icing.end(), icing.begin(), ::tolower);
+    toppingPrompt(icing);
+    std::cin >> std::ws;
+    std::getline(std::cin, top);
+    std::string topCopy = top;
+    std::transform(topCopy.begin(), topCopy.end(), topCopy.begin(), ::tolower);
+    if (icing == "none")
+    {
+        while (topCopy != "cinnamon sugar" && topCopy != "powdered sugar" && topCopy != "none")
+        {
+            codeGradeLoopFix("main.cpp::getTopping(\"none\")");
+            std::cout << top << "is not valid for icing " << icing << std::endl;
+            toppingPrompt(icing);
+            std::getline(std::cin, top);
+            topCopy = top;
+            std::transform(topCopy.begin(), topCopy.end(), topCopy.begin(), ::tolower);
+        }
+    }
+    else
+    {
+        while (!Donut::strToTop.count(topCopy))
+        {
+            codeGradeLoopFix("main.cpp::getTopping()");
+            std::cout << top << " is not a valid topping." << std::endl;
+            toppingPrompt(icing);
+            std::getline(std::cin, top);
+            topCopy = top;
+            std::transform(topCopy.begin(), topCopy.end(), topCopy.begin(), ::tolower);
+        }
+    }
+    return top;
+}
+
+void icingPrompt()
+{
+    std::cout << "Choose your icing from the choices below: " << std::endl;
+    for (std::map<Donut::icingType, std::string>::const_iterator it = Donut::iceToStr.begin(); it != Donut::iceToStr.end(); ++it)
+    {
+        std::cout << it->second << std::endl;
+    }
+}
+
+std::string getDrizzle()
+{
+    std::string drizzle;
+    drizzlePrompt();
+    std::cin >> std::ws;
+    std::getline(std::cin, drizzle);
+    std::string drizzleLower = drizzle;
+    std::transform(drizzleLower.begin(), drizzleLower.end(), drizzleLower.begin(), ::tolower);
+    while (!Donut::strToDrizzle.count(drizzleLower))
+    {
+        codeGradeLoopFix("main.cpp::getDrizzle()");
+        std::cout << drizzle << " is not a valid drizzle choice." << std::endl;
+        drizzlePrompt();
+        std::cin >> std::ws;
+        std::getline(std::cin, drizzle);
+        drizzleLower = drizzle;
+        std::transform(drizzleLower.begin(), drizzleLower.end(), drizzleLower.begin(), ::tolower);
+    }
+    return drizzle;
+}
+
+void drizzlePrompt()
+{
+    std::cout << "Choose your drizzle from the choices below:" << std::endl;
+    for (std::map<Donut::drizzleType, std::string>::const_iterator it = Donut::drizzleToStr.begin(); it != Donut::drizzleToStr.end(); ++it)
+    {
+        std::cout << it->second << std::endl;
+    }
+}
+
+void toppingPrompt(std::string icing)
+{
+    std::cout << "Choose your topping from the choices below:" << std::endl;
+    if (icing == "none")
+    {
+        std::cout << "Cinnamon Sugar" << std::endl;
+        std::cout << "Powdered Sugar" << std::endl;
+        std::cout << "None" << std::endl;
+    }
+    else
+    {
+        for (auto it = Donut::topToStr.begin(); it != Donut::topToStr.end(); ++it)
+        {
+            std::cout << it->second << std::endl;
+        }
+    }
+}
+
+void referencePointers(Donut *&p, Donut *d)
+{
+    Donut *temp = p;
+    p = d;
+    d->setTopping("chocolate chips");
+
+    d = temp;
+    // delete temp;
 }
--- 06_11_26/order.cpp
+++ 06_11_26/order.cpp
@@ -5,6 +5,21 @@ Order::Order()
     donutList = nullptr;
     numDonuts = 0;
 }
+Order::Order(const Order &otherOrder)
+{
+    donutList = nullptr;
+    numDonuts = 0;
+    copyOrder(otherOrder);
+}
+
+const Order &Order::operator=(const Order &otherOrder)
+{
+    if (this != &otherOrder)
+    {
+        copyOrder(otherOrder);
+    }
+    return *this;
+}
 
 void Order::operator+(const Donut &d)
 {
@@ -34,4 +49,27 @@ void Order::destroyList()
 {
     delete[] donutList;
     donutList = nullptr;
+}
+
+void Order::copyOrder(const Order &otherOrder)
+{
+    if (this->donutList != nullptr) //*(this).
+    {
+        destroyList();
+    }
+    this->numDonuts = otherOrder.numDonuts;
+    this->donutList = new Donut[this->numDonuts];
+    for (int i = 0; i < this->numDonuts; i++)
+    {
+        donutList[i] = otherOrder.donutList[i];
+    }
+}
+
+std::ostream &operator<<(std::ostream &out, const Order &o)
+{
+    for (int i = 0; i < o.numDonuts; i++)
+    {
+        out << o.donutList[i] << std::endl;
+    }
+    return out;
 }
