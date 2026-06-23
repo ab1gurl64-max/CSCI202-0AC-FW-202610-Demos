e9ba569365494453d67b72155797f817e4d5ae60
Angela Venable <avenable11@ivytech.edu>
2026-06-23 15:54:49 -0400

    with lab activity

---
+++ 06_23_26/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <regex>
+#include <cmath>
+#include "arrayStack.h"
+#include "linkedStack.h"
+
+// M03-a Lab come up with 5 binary numbers of test data for the converter
+// also come up with 5 hex numbers
+std::string inputBinaryNumber();
+int convertBinaryToDecimal(StackADT<int> &);
+int convertDecimalToBinary(StackADT<int> &, int);
+std::string inputHexadecimalNumber();
+int convertHexToDecimal(StackADT<int> &);
+
+int main()
+{
+
+    return 0;
+}
